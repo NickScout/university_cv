b91dda4e61439c70b3d4bdc402a95d1638077e13
nickscout@protonmail.com <344454Tt>
2019-07-03 19:32:10 +0300

    department interface

--- src/main/java/com/nickscout/university/model/DepartmentInterface.java
+++ src/main/java/com/nickscout/university/model/DepartmentInterface.java
@@ -1,4 +1,11 @@
 package com.nickscout.university.model;
 
+import java.util.Set;
+
 public interface DepartmentInterface {
+    Lector getHead();
+    Set<Lector> getAllLectors();
+    Set<Lector> getAllLectorsByDegree(Degree degree);
+    double getAverageSalary();
+    int countLectors();
 }
