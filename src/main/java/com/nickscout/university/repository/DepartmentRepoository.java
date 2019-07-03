f818be88117a07ad0510423f38e6334d5d904960
nickscout@protonmail.com <344454Tt>
2019-07-03 19:34:07 +0300

    deleted mess

--- src/main/java/com/nickscout/university/repository/DepartmentRepoository.java
+++
@@ -1,9 +0,0 @@
-package com.nickscout.university.repository;
-
-import com.nickscout.university.model.Department;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DepartmentRepoository extends JpaRepository<Department, Long> {
-}
