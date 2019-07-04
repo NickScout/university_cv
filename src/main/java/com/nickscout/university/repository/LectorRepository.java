432a9f67e316c09294a4905955985656689edf03
nickscout@protonmail.com <344454Tt>
2019-07-04 13:29:11 +0300

    added Lector REST API

--- src/main/java/com/nickscout/university/repository/LectorRepository.java
+++ src/main/java/com/nickscout/university/repository/LectorRepository.java
@@ -12,4 +12,7 @@ public interface LectorRepository extends JpaRepository<Lector, Long> {
     @Query(value = "select l from Lector l where locate(?1, l.firstName) > 0 or locate(?1, l.secondName) > 0")
     Set<Lector> findLectorsByPartialName(String name);
 
+    @Query(value = "SELECT * FROM department d LIMIT ?1",
+    nativeQuery = true)
+    Set<Lector> findAllWithLimit(int limit);
 }
